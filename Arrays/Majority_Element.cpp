062f05a89f085f040ce114dc885e4ce460026f8b
Mohit Kumar <mohitvkumar.in@gmail.com>
2026-03-17 23:54:23 +0530

    Added Majority Element solution using Moore's Voting
    Implemented Moore's Voting algorithm to find the majority element in an array.

---
+++ Arrays/Majority_Element.cpp
@@ -0,0 +1,23 @@
+// Problem : Majority Element
+// Approach: Moore's Voting
+// TC : O(n)
+// SC : O(1)
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n= nums.size();
+        int freq=0 ; int ans=0;
+        for(int i=0 ; i<n ; i++){
+            if(freq==0){
+                ans=nums[i];
+            }
+            if(nums[i]==ans){
+                freq++;
+            }else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
