d60d09ad3fb7fc6ce8fb0f25c075f4e9e0139127
Mohit Kumar <mohitvkumar.in@gmail.com>
2026-04-16 23:13:38 +0530

    Added solution for Longest Common Prefix problem
    Implement solution for finding the longest common prefix among strings using sorting.

---
+++ Strings/07_Longest_Common_Prefix.cpp
@@ -0,0 +1,36 @@
+// problem : Longest Common Prefix
+// TC : O(nlogn + m)
+// SC : O(1)
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+        if(strs.size()==0){
+            return "";
+        }
+        sort(strs.begin(), strs.end());
+
+        string first= strs[0];
+        string last= strs[strs.size()-1];
+        
+        string ans="";
+
+        int minlen= min(first.size(),last.size());
+
+        for(int i=0 ; i<minlen ; i++){
+            if(first[i]!=last[i]){
+              
+               return ans;
+            }
+           
+           ans += first[i];
+
+                
+            
+        }
+        return ans;
+
+    }
+};
